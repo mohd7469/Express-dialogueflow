b3baee52740d955d3ec6c4c2ee06df999c98a894
awais786327 <awais3227443@gmail.com>
2018-09-29 15:58:06 +0500

    added fulfilment

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const { WebhookClient } = require('dialogflow-fulfillment');
 
 router.get('/', function(req, res, next) {
   res.send('Hello from express /');
 });
+
+router.post('/dialogflowExpressFulfilment', function(req, res, next) {
+  WebhookProcessing(req, res);
+});
+
+function WebhookProcessing(request, response) {
+  const agent = new WebhookClient({ request, response });
+
+  function getNotification(agent) {
+    agent.add('login init good')
+  }
+
+  let intentMap = new Map();
+  intentMap.set('Get Notification', getNotification);
+  agent.handleRequest(intentMap);
+}
 
 module.exports = router;

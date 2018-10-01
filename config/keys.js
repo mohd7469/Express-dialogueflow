9ef32876c77d1596f2f6e12c9e8cd2fbcded0ab8
awais786327 <awais3227443@gmail.com>
2018-10-01 14:15:01 +0500

    uncommit keys

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  facebook: {
+    clientID: "502268686919048",
+    clientSecret: "0c255bcd44df8c55e3bf77296782c716"
+  },
+  session: {
+    cookieKey: 'thisisveryawesomedialogflownodejsexpressfulfilmentcode'
+  }
+};
